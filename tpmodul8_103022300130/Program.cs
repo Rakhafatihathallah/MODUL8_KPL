c460759439dfd6bc002776459a83beaf144f63c4
Rakha Fatih <rakhafatihathallah@gmail.com>
2025-04-14 00:17:42 +0700

    First Commit - Project Initialization

---
+++ tpmodul8_103022300130/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tpmodul8_103022300130
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
